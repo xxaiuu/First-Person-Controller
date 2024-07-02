54c374b14b8845babf604d13d748d53f5b1cabdb
Alex Liu <alex.stillcravingfood@gmail.com>
2024-07-02 00:16:49 -0700

    sprint and fov

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # First-Person-Controller
 Learning more about Godot 4 with a youtube tutorial, "Juiced Up First Person Character Controller Tutorial - Godot 3D FPS", using C#
+
+
+About this project:
+- 3D Godot 4 in C#
+- First Person
+- Look around available  (must turn off head bob)
+- Head Bob available (default)
+- walk and sprinting available
+- inertia movement such that players do not freeze in place in the air if they don't hold down the movement key
+- FOV available (when sprinting, the screen will change to indicate faster running)
--- scripts/player.cs
+++ scripts/player.cs
@@ -3,19 +3,32 @@ using System;
 
 public partial class player : CharacterBody3D
 {
-	public const float Speed = 5.0f;
+	// Movement related Variables
+	public const float WalkSpeed = 5.0f;
+	public const float SprintSpeed = 8.0f;
 	public const float JumpVelocity = 4.5f;
 	public const float Sensitivity = 0.01f;	//look sensitivity
 
+	// Bob Variables
 	public const float BobFrequency = 2.0f;	//how often to bob the head
 	public const float BobAmp = 0.08f;	//how far up and down the camera goes
-	
 	private float t_bob = 0.0f;	//how far along the sine wave (bobbing motion)
+
+	// FOV Variables
+	public float Base_FOV = 75.0f;
+	private float FOV_Change = 1.5f;	//multiple our speed and add to our base FOV
+
+	// Private Variables
+	private bool use_bob = true;
+	private bool use_fov = true;
 	private Node3D head;
 	private Camera3D camera;
-	
+	private float speed;
+
+
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+
 
 	//Hides mouse once loaded in
 	public override void _Ready()
@@ -33,7 +46,9 @@ public partial class player : CharacterBody3D
 
 		if(@event is InputEventMouseMotion mouseMotion){
 			head.RotateY(-mouseMotion.Relative.X * Sensitivity);	//due to the translation between mouse and camera
-			RotateY(-mouseMotion.Relative.X * Sensitivity);	//rotates whole
+			
+			if(use_bob)
+				RotateY(-mouseMotion.Relative.X * Sensitivity);	//rotates whole	
 			
 			camera.RotateX(-mouseMotion.Relative.Y * Sensitivity);
 
@@ -44,7 +59,7 @@ public partial class player : CharacterBody3D
 
 				Vector3 currentRotation = camera.RotationDegrees;
 				float newRotationX = currentRotation.X + deltaX;
-				float clampedX = Math.Clamp(newRotationX, minAngleDegrees, maxAngleDegrees);
+				float clampedX = Mathf.Clamp(newRotationX, minAngleDegrees, maxAngleDegrees);
 				camera.RotationDegrees = new Vector3(clampedX, currentRotation.Y, currentRotation.Z);
 			}
 		}
@@ -67,25 +82,55 @@ public partial class player : CharacterBody3D
 		if (Input.IsActionJustPressed("jump") && IsOnFloor())
 			velocity.Y = JumpVelocity;
 
+		// Handle speed
+		if (Input.IsActionPressed("sprint"))
+			speed = SprintSpeed;
+		else
+			speed = WalkSpeed;
+
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 inputDir = Input.GetVector("left", "right", "up", "down");
 		Vector3 direction = (head.Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
+
+		if (IsOnFloor()){
+			if (direction != Vector3.Zero)
+			{
+				velocity.X = direction.X * speed;
+				velocity.Z = direction.Z * speed;
+			}
+			else
+			{
+				//You will glide a little, to do a complete stop instantly, change these to 0
+				velocity.X = Mathf.Lerp(velocity.X, direction.X * speed, (float)(delta * 7.0));	
+				velocity.Z = Mathf.Lerp(velocity.Z, direction.Z * speed, (float)(delta * 7.0));
+			}
 		}
-		else
-		{
-			velocity.X = 0; 
-			velocity.Z = 0;
+		else {
+			//initial velocity, target velocity, decimal percentage between the two variables we want to cover in each step
+			velocity.X = Mathf.Lerp(velocity.X, direction.X * speed, (float)(delta * 2.0));	//delta usually 0.05 so this is actually 10%
+			velocity.Z = Mathf.Lerp(velocity.Z, direction.Z * speed, (float)(delta * 2.0));
 		}
 
-		
+
+
+		// NOTE: looking up and down no longer works when using head bob (which makes sense, can't bob head while looking at the sky can we?)
+		//			disable bob code for exploration type first person
+		//			Head Bob is more suited for a linear path running - realistic minus not looking up
 						// time 	*	speed
-		t_bob += (float)delta * (float)velocity.Length() * (IsOnFloor() ? 1f : 0f);
-		UpdateCameraPosition(t_bob);
+		if(use_bob){
+			t_bob += (float)delta * (float)velocity.Length() * (IsOnFloor() ? 1f : 0f);
+			UpdateCameraPosition(t_bob);
+		}
+
+		// FOV
+		if (use_fov){
+			var velocity_clamp = Mathf.Clamp(velocity.Length(), 0.5, SprintSpeed * 2.0f);
+			var target_fov = Base_FOV + FOV_Change * velocity_clamp;	//clamp such that our FOV doesn't go too crazy
+
+			camera.Fov = Mathf.Lerp(camera.Fov, (float)target_fov, (float)delta * 8.0f);
+		}
+
 
 		Velocity = velocity;
 		MoveAndSlide();
@@ -97,6 +142,7 @@ public partial class player : CharacterBody3D
     {
         Vector3 pos = Vector3.Zero;
         pos.Y = Mathf.Sin(time * BobFrequency) * BobAmp;
+		pos.X = Mathf.Cos(time * BobFrequency/2) * BobAmp;	//I can't really tell the difference 
         return pos;
     }
 
